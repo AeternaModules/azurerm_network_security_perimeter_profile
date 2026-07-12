2d4778d56f04ede798ced1167cb5242ae2a8bb32
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "network_security_perimeter_profiles" {
-  description = "All network_security_perimeter_profile resources"
-  value       = azurerm_network_security_perimeter_profile.network_security_perimeter_profiles
-}
 output "network_security_perimeter_profiles_name" {
-  description = "List of name values across all network_security_perimeter_profiles"
-  value       = [for k, v in azurerm_network_security_perimeter_profile.network_security_perimeter_profiles : v.name]
+  description = "Map of name values across all network_security_perimeter_profiles, keyed the same as var.network_security_perimeter_profiles"
+  value       = { for k, v in azurerm_network_security_perimeter_profile.network_security_perimeter_profiles : k => v.name }
 }
 output "network_security_perimeter_profiles_network_security_perimeter_id" {
-  description = "List of network_security_perimeter_id values across all network_security_perimeter_profiles"
-  value       = [for k, v in azurerm_network_security_perimeter_profile.network_security_perimeter_profiles : v.network_security_perimeter_id]
+  description = "Map of network_security_perimeter_id values across all network_security_perimeter_profiles, keyed the same as var.network_security_perimeter_profiles"
+  value       = { for k, v in azurerm_network_security_perimeter_profile.network_security_perimeter_profiles : k => v.network_security_perimeter_id }
 }
 
